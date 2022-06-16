f940d8ea8d96f286c5e6a3172ce47ef2b846c6fa
PedroHercules <pedroherculesdantas@gmail.com>
2022-06-16 14:50:06 -0300

    fix error syntax in app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,7 +3,7 @@ import { UsersModule } from './users/users.module';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ConfigModule } from '@nestjs/config';
 
-ConfigModule.forRoot(),
+ConfigModule.forRoot();
 
 @Module({
   imports: [
